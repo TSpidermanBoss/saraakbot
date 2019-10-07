0472488f1eac5b047280f0ff37ba73128e9d1c0a
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-07 14:54:08 +0530

    Update extra.py

--- tg_bot/modules/extra.py
+++ tg_bot/modules/extra.py
@@ -2,7 +2,7 @@ import random, re
 from random import randint
 from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
-from telegram.ext import Filters, MessageHandler, run_async
+from telegram.ext import Filters, MessageHandler, run_async, loggable, user_admin
 
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
